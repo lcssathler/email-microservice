98a801cdd21768791fe4c359b71e1db8a7ddfbd5
lcssathler <lcssathler@gmail.com>
2025-05-27 22:09:28 -0300

    add: user model

---
+++ user/src/main/java/com/ms/user/models/User.java
@@ -0,0 +1,38 @@
+package com.ms.user.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String email;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
